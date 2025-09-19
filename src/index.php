aa300654133fde3eb1d665371ebdbfa864554489
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-19 15:22:54 +0800

    add frontend domain in env variable:

--- src/index.php
+++ src/index.php
@@ -1,7 +1,13 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$frontend_domain = $_ENV['FRONTEND_DOMAIN'];
+
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: ' . $frontend_domain);
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
