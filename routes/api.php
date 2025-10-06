71b92946b0b337b6d2e873b4f48bdccd422e761b
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-06 22:07:56 +0800

    alter user table

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/tt1', function(){
+    return "hello tt1";
+});
