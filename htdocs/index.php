fe8119af0a8b849cfc1677d3ba9eaa6233c5612b
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-07 16:34:31 +0800

    setup backend credentials

---
+++ htdocs/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$frontend_domain = $_ENV['FRONTEND_DOMAIN'];
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: ' . $frontend_domain);
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(200);
+}
