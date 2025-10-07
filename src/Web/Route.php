33afc4ccb8bd6bd6adffcf850b22ffcc417bcfef
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-07 20:14:15 +0800

    change router mechanism, moved logic to index.php

--- src/Web/Route.php
+++ src/Web/Route.php
@@ -1,38 +1,10 @@
 <?php
 
-namespace App\Web;
 use App\Controller\AuthController;
-use App\Http\Response;
 
-class Route {
-    private static array $routes = [];
+$routes = [];
 
-    public static function initRoutes()
-    {
-        self::$routes["POST"] = [
-            "/auth/login" => [AuthController::class, "login"],
-            "/auth/register" => [AuthController::class, "register"]
-        ];
-    }
-
-    public static function handleRoute($url, $method){
-        // Ensure routes are initialized before use
-        if (empty(self::$routes)) {
-            self::initRoutes();
-        }
-
-        if (!isset(self::$routes[$method][$url])) {
-            Response::sendJson(
-                404,
-                false,
-                "File not found",
-                null,
-            );
-        }
-    
-        [ $controller, $method ] = self::$routes[$method][$url];
-    
-        $controller = new $controller();
-        $controller->$method();
-    }
-}
+$routes["POST"] = [
+    "/auth/register" => [ AuthController::class, "register" ],
+    "/auth/login" => [ AuthController::class, "login" ] 
+];
